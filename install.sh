f09d8867176ba7c1e73279ab772b42755cb09341
Morteza <mortazapor3@gmail.com>
2023-09-09 06:46:07 +0330

    Pingme

--- install.sh
+++ install.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
 function install_scripts(){
-    local SCRIPTS=("Node package installer (npi)")
+    local SCRIPTS=("Node package installer (npi)" "Ping Utility (pingme)")
     local PS3="Select the package you want to install: "
 
-    select SCRIPT in "$SCRIPTS"
+    select SCRIPT in "${SCRIPTS[@]}"
     do
         case $REPLY in
             1)
-                sudo cp ./npi /usr/local/bin 
+                sudo cp ./npi /usr/bin 
                 echo 'Use the command "npi" to use the package !'
+                exit 0
+                ;;
+            2)
+                sudo cp ./pingme /usr/bin 
+                echo 'Use the command "pingme" to use the package !'
                 exit 0
                 ;;
             *)
